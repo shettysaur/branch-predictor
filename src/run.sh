efcf9318cc5e66b5985f93dc93d17c8606f9ef6f
Saurabh Shetty <saurabh.shetty.96@gmail.com>
2021-11-05 22:47:36 -0700

    Moving from C to c++

---
+++ src/run.sh
@@ -0,0 +1,14 @@
+#make clean
+#make all
+FILES="../traces/*"
+for (( g=12; g<=20; g=g+2 ))
+	do
+	echo "Processing $g bits"
+	for f in $FILES
+		do
+		out="$(basename -s .bz2 $f)"
+		echo "Processing $out file..."
+		out_file="$out"
+		bunzip2 -kc ../traces/$f | ./predictor --gshare:$g >> ../out/$out_file &
+		done
+	done
